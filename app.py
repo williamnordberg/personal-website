1b55143460ea0a6ee2bd8c04038f35bc738587fb
will <william.nordberg3@gmail.com>
2023-12-26 10:26:42 +0100

    make the app constantly running

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-"""all test shall pass 4 """
-print('app running and ready to deploy2')
+"""just print a simple massage"""
+
+from time import sleep
+
+# all test shall pass 4
+while True:
+    print('app running and ready to deploy2')
+    sleep(5)
